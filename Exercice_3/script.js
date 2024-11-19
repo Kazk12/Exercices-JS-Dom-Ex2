535be8b88758e8c0c611c5f1fdf6f1be157e07e9
Kazk12 <kazuko43KO@gmail.com>
2024-11-19 09:32:14 +0100

    ADD : Exo3 1 commentaire

--- Exercice_3/script.js
+++ Exercice_3/script.js
@@ -1,6 +1,6 @@
 const lastname = document.querySelector("#lastname");
 
-lastname.addEventListener("keyup", handleKeyupMontreValeur);
+lastname.addEventListener("keyup", handleKeyupMontreValeur); // Ou input(quand on met un truc dans un textarea et autre chose) à la place de keyup
 
 function handleKeyupMontreValeur(event){
     console.log(event.target.value);
